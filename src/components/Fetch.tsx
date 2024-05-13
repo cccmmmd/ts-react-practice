010dea3d0a02d1764de083a7f24f78549fe36238
cccmmmd <bblisme8b@gmail.com>
2024-05-13 18:43:39 +0800

    fetch api

---
+++ src/components/Fetch.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+type CommentType = {
+    postId: number
+    id: number
+    name: string
+    email: string
+    body: string
+}
+const App:React.FC = () => {
+    const [postId, setId] = useState<number>(1)
+    const [error, setError] = useState<Error | null>(null)
+    const [loading, setLoading] = useState(false)
+
+    
+    async function fetchData(id:number){
+        setLoading(true)
+        try {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+            const data = await res.json() as CommentType[]
+            console.log(`data${id}`, data)
+        } catch (error) {
+            setError(error as Error)
+        }
+        setLoading(false)
+    }
+
+    useEffect(()=>{
+        fetchData(postId)
+        // fetch('https://jsonplaceholder.typicode.com/comments?postId=1')
+        // .then(res => res.json())
+        // .then((data:CommentType[]) => {
+        //     console.log('data2', data)
+        // })
+
+    },[postId])
+
+    const clickHandler = (id: number) => {
+        setId(id)
+    }
+    return <>
+        <h1>Fetch</h1>
+        <button onClick={() => clickHandler(2)}>fetch id 2 data</button>
+        <button onClick={() => clickHandler(3)}>fetch id 3 data</button>
+        {
+            error === null? <p style={{color:'green'}}>資料獲取成功</p>:<p style={{color:'red'}}>資料獲取失敗</p>
+        }
+        {
+            loading? <h1>loading</h1>: null
+        }
+    </>
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import Hook from './components/Hook-hoc'
 import Useffect from './components/Useffect'
+import Fetch from './components/Fetch'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Useffect />
+    <Fetch/>
+    {/* <Useffect /> */}
     <Hook />
     <App />
   </React.StrictMode>
